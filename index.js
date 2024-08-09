9352173a1f580336537647bed547e831d7908774
veeru <veeru@gmail.com>
2024-08-09 13:37:45 +0530

    solution

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const UsersModel = require("./module/Users");
+app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+mongoose.connect("mongodb://localhost:27017/usersData");
+
+app.listen(3001, () => {
+  console.log("Server Running at http://localhost:3001");
+});
+
+app.get("/", async (req, res) => {
+  res.json("Hello");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, name, email, password } = req.body;
+  const checkUser = await UsersModel.findOne({ username: username });
+  if (checkUser) {
+    res.status(400);
+    res.json("User already exists");
+  } else {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    UsersModel.insertMany({
+      username,
+      name,
+      email,
+      password: hashedPassword,
+    });
+    res.status(200);
+    res.json("User Created Successfully");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UsersModel.findOne({ username });
+  if (username === "" || password === "") {
+    res.status(400);
+    res.json("Bad Request");
+  }
+  if (!user) {
+    res.status(400);
+    res.json("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+    if (isPasswordMatched === true) {
+      const payload = { username: username };
+      const jwtToken = jwt.sign(payload, "dgfhefbrker");
+      res.send({ jwtToken });
+    } else {
+      res.status(400);
+      res.json("Invalid Password");
+    }
+  }
+});
